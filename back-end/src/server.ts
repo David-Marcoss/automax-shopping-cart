c377f6a5707e4cfbebdce955cbbe5746e816275d
david-marcoss <ddragondavi@gmail.com>
2026-02-21 14:10:23 -0300

    config: config cors on server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,18 +1,10 @@
 import express from "express";
-import { prisma } from "./database/prisma";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "API rodando 🚀" });
-});
-
-app.get("/carts", async (req, res) => {
-  const carts = await prisma.carts.findMany();
-  res.json(carts);
-});
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("🔥 Server rodando em http://localhost:3000");
