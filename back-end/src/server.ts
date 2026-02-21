bfaa3b5cc01f0a17b893d811107adad62abb3f4e
david-marcoss <ddragondavi@gmail.com>
2026-02-21 11:25:36 -0300

    config: add initial files and config to back-end

---
+++ back-end/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { prisma } from "./database/prisma";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API rodando 🚀" });
+});
+
+app.get("/carts", async (req, res) => {
+  const carts = await prisma.carts.findMany();
+  res.json(carts);
+});
+
+app.listen(3000, () => {
+  console.log("🔥 Server rodando em http://localhost:3000");
+});
